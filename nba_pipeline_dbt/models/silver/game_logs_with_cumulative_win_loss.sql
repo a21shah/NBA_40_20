b4bc737409c6729b45745a6394a4958c96392cb3
Abhishek Shah <36013070+a21shah@users.noreply.github.com>
2026-06-26 14:52:41 -0400

    Enhance game logs with win/loss indicators
    Added selection of game details and win/loss indicators from bronze.game logs

--- nba_pipeline_dbt/models/silver/game_logs_with_cumulative_win_loss.sql
+++ nba_pipeline_dbt/models/silver/game_logs_with_cumulative_win_loss.sql
@@ -1,7 +1,18 @@
 {{ config(materialized='table') }}
 
 with source as (
-    select * from {{ ref('game_logs') }}
+    select
+        game_id,
+        wl,
+        year,
+        season_id,
+        team_id,
+        team_abbreviation,
+        team_name,
+        game_date::date as game_date,
+        case when wl = 'W' then 1 else 0 end as is_win,
+        case when wl = 'L' then 1 else 0 end as is_loss
+    from {{ source('bronze', 'game_logs') }}
 ),
 
 with_cumulative as (
